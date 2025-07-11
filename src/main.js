f687f8fe748e53d8aefbd4b4b60c886906ffe057
Vit <vituhy@gmail.com>
2025-07-11 15:00:14 +0200

    field access

--- src/main.js
+++ src/main.js
@@ -125,7 +125,7 @@ const crawler = new HttpCrawler({
                 days: days
             });
             await Actor.setValue('data2-debug.json', JSON.stringify(data2, null, 2), { contentType: 'application/json' });
-            await Actor.setValue(`response-${request.uniqueKey}.txt`, output, { contentType: 'text/plain' });
+            await Actor.setValue(`response-${request.id}.txt`, output, { contentType: 'text/plain' });
         } else {
             log.error('No target line found in the response');
         }
